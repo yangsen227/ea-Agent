540bd46038bca4eaf3630bd4ea142a463ca653cd
yangsen227 <64386759+yangsen227@users.noreply.github.com>
2026-01-21 11:52:50 +0800

    [20260121]核心组件项目新建

---
+++ ea-core/src/main/java/com/eacore/EaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.eacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaCoreApplication.class, args);
+    }
+
+}
